79190761ba1ded1c985614c9f86a7727ba303b80
LuchoSantini <lucho_santini7@outlook.es>
2024-04-12 18:47:56 -0300

    added get & put

---
+++ To-Do_List-LC4/Data/Dtos/ToDoItemToEditDto.cs
@@ -0,0 +1,9 @@
+﻿namespace To_Do_List_LC4.Data.Dtos
+{
+    public class ToDoItemToEditDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ To-Do_List-LC4/Data/Dtos/UserToEditDto.cs
@@ -0,0 +1,10 @@
+﻿namespace To_Do_List_LC4.Data.Dtos
+{
+    public class UserToEditDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
